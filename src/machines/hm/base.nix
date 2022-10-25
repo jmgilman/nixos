b34a2493adee8a6d5d7db6a86166e53871adc278
Joshua Gilman <joshuagilman@gmail.com>
2022-10-25 13:12:29 -0700

    fix: minor tmux tweaks

--- src/machines/hm/base.nix
+++ src/machines/hm/base.nix
@@ -49,7 +49,12 @@
     enable = true;
 
     prefix = "C-a";
+    baseIndex = 1;
     terminal = "screen-256color";
+
+    extraConfig = ''
+      bind m set-window-option main-pane-height 60\; select-layout main-horizontal
+    '';
 
     tmuxp.enable = true;
   };
