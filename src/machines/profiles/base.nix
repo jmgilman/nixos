42e544e5f66a74141f030e7a0ad31680d15b4a93
Joshua Gilman <joshuagilman@gmail.com>
2022-10-25 11:03:04 -0700

    fix: fixes timezone

--- src/machines/profiles/base.nix
+++ src/machines/profiles/base.nix
@@ -6,6 +6,9 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
+  # Set timezone to PST
+  time.timeZone = "America/Los_Angeles";
+
   users.users.${defaultUser} = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
