45e10d6e6a629e909e2c3b7d6647bed818f47d39
Joshua Gilman <joshuagilman@gmail.com>
2022-10-25 02:49:06 +0000

    feat: adds cargo-edit

--- src/dev/profiles/lang/rust.nix
+++ src/dev/profiles/lang/rust.nix
@@ -1,5 +1,9 @@
 { inputs, pkgs, ... }:
 {
   nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
-  environment.systemPackages = with pkgs; [ rust-bin.stable.latest.default ];
+  environment.systemPackages = with pkgs;
+    [
+      cargo-edit
+      rust-bin.stable.latest.default
+    ];
 }
