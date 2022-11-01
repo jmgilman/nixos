ae995087e95e5a582b2cc88410877ecf62e745c6
Joshua Gilman <joshuagilman@gmail.com>
2022-11-01 14:02:03 -0700

    fix: uses home-manager for configuring gpg-agent

--- src/security/hm/gpg.nix
+++ src/security/hm/gpg.nix
@@ -3,4 +3,10 @@
     enable = true;
     publicKeys = [{ source = ../files/gpg; }];
   };
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 3600;
+    enableExtraSocket = true;
+    pinentryFlavor = "curses";
+  };
 }
--- src/security/profiles/gpg.nix
+++ src/security/profiles/gpg.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [ gnupg yubikey-manager ];
-  
+
   services.pcscd.enable = true;
-  programs.gnupg.agent = { 
-    enable = true; 
-    pinentryFlavor = "curses";
-  };
 }
