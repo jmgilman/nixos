d26fecb4aa0d57c835376b3a2619c86c14f2b72e
Joshua Gilman <joshuagilman@gmail.com>
2022-12-29 22:34:40 -0800

    refactor: changes method for working with aws-vault

--- src/machines/hm/base.nix
+++ src/machines/hm/base.nix
@@ -1,12 +1,112 @@
 {inputs}: {pkgs, ...}: let
   inherit (inputs.tree-grepper.packages) tree-grepper;
 
-  # Custom scripts
-  aws-vault-login = pkgs.writeShellScript "aws-vault-login" ''
-    auth="$(aws-vault exec "$AWS_PROFILE" -t "$(gopass otp -o aws/mfa)" -j)"
-    export AWS_ACCESS_KEY_ID="$(echo "$auth" | jq -r .AccessKeyId)"
-    export AWS_SECRET_ACCESS_KEY="$(echo "$auth" | jq -r .SecretAccessKey)"
-    export AWS_SESSION_TOKEN="$(echo "$auth" | jq -r .SessionToken)"
+  # Custom scripts for aws-vault
+  aws-vault-cred = pkgs.writeShellScript "aws-vault-login" ''
+    if [[ -z "$1" ]]; then
+      echo "error: Missing profile name"
+      exit 1
+    fi
+
+    # Verify a profile is configured
+    if ! aws-vault list --profiles | grep "$1" > /dev/null 2>&1; then
+      echo "error: profile $1 has not been configured"
+      exit 1
+    fi
+
+    # Check for virtual MFA
+    mfa=$(aws configure get mfa_serial --profile "$1")
+
+    if [[ ! -z "$mfa" ]]; then
+      if ! gopass show -y "aws/mfa-$1" > /dev/null 2>&1; then
+        echo "error: profile $1 has MFA configured but has no entry in gopass"
+        exit 1
+      fi
+    fi
+
+    # Return auth details
+    if [[ ! -z "$mfa" ]]; then
+      aws-vault exec -j -t "$(gopass otp -o aws/mfa-$1)" "$1"
+    else
+      aws-vault exec -j -n "$1"
+    fi
+  '';
+
+  aws-vault-add = pkgs.writeShellScript "aws-vault-login" ''
+    if [[ -z "$1" ]]; then
+      echo "error: Missing profile name"
+      exit 1
+    fi
+
+    # Check if profile is already configured
+    if aws-vault list --profiles | grep "$1"; then
+      read -p "Profile exists; reconfigure it? [y/n] " -n 1 -r
+      echo
+      if [[ $REPLY =~ ^[Yy]$ ]]
+      then
+        aws-vault remove -f "$1"
+      fi
+    fi
+
+    # Ask for authentication details
+    echo -n "Access Key ID: "
+    read -s AWS_ACCESS_KEY_ID
+    echo
+
+    echo -n "Secret Key: "
+    read -s AWS_SECRET_ACCESS_KEY
+    echo
+
+    # Add credentials
+    export AWS_ACCESS_KEY_ID
+    export AWS_SECRET_ACCESS_KEY
+    aws-vault add --env "$1"
+
+    # Configure the profile
+    aws configure set credential_process "${aws-vault-cred} $1" --profile "$1"
+  '';
+
+  aws-vault-add-mfa = pkgs.writeShellScript "aws-vault-login" ''
+    if [[ -z "$1" ]]; then
+      echo "error: Missing profile name"
+      exit 1
+    fi
+
+    if [[ -z "$2" ]]; then
+      echo "error: Missing username"
+      exit 1
+    fi
+
+    echo ">>> Registering MFA device..."
+    serial=$(aws iam create-virtual-mfa-device \
+      --virtual-mfa-device-name gopass \
+      --outfile /tmp/creds \
+      --bootstrap-method Base32StringSeed | jq -r .VirtualMFADevice.SerialNumber)
+
+    echo ">>> Saving secret key..."
+    cat /tmp/creds | gopass insert -f "aws/mfa-$1"
+    rm -rf /tmp/creds
+
+    echo ">>> Generating OTP #1..."
+    otp1=$(gopass otp -o "aws/mfa-$1")
+
+    echo ">>> Waiting 35 seconds..."
+    sleep 35
+
+    echo ">>> Generating OTP #2..."
+    otp2=$(gopass otp -o "aws/mfa-$1")
+
+    echo ">>> Attaching MFA..."
+    aws iam enable-mfa-device \
+      --user-name "$2" \
+      --serial-number "$serial" \
+      --authentication-code1 "$otp1" \
+      --authentication-code2 "$otp2"
+
+    echo ">>> Updating profile..."
+    aws configure set mfa_serial --profile "$1" "$serial"
+
+    echo ">>> Done!"
   '';
 in {
   home.stateVersion = "22.05";
@@ -30,8 +130,10 @@ in {
       exa
       fd
       fzf
+      gh
       gopass
       helix
+      helm
       httpie
       jc
       jq
@@ -105,7 +207,9 @@ in {
     shellAliases = {
       ".." = "cd ..";
       "..." = "cd ../..";
-      aws-vault-login = "source ${aws-vault-login}";
+      aws-vault-cred = "${aws-vault-cred}";
+      aws-vault-add = "${aws-vault-add}";
+      aws-vault-add-mfa = "${aws-vault-add-mfa}";
       brg = "batgrep";
       cat = "bat --paging=never";
       count = "find . -type f | wc -l";
@@ -114,6 +218,8 @@ in {
       dr = "direnv reload";
       g = "git";
       gpb = ''git push origin "$(git rev-parse --abbrev-ref HEAD)"'';
+      h = "helm";
+      k = "kubectl";
       left = "ls -t -1";
       ls = "exa";
       ll = "exa -la";
@@ -123,6 +229,7 @@ in {
       now = ''date +"%T"'';
       nr = "sudo nixos-rebuild switch --flake ~/code/nixos#Office && source ~/.zshrc";
       ports = "sudo lsof -iTCP -sTCP:LISTEN -n -P";
+      reload = "source ~/.zshenv; source ~/.zshrc";
       t = "tmux";
       ta = "tmux attach";
       tf = "terraform";
